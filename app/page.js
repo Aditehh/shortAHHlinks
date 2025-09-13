8e108b83c9b460a7f230ba6c6cbe108aeaa7b6b3
aditehh <aditya.aryal888@gmail.com>
2025-09-13 21:34:56 +0545

    thisandthatandthisandthat

--- app/page.js
+++ app/page.js
@@ -23,7 +23,9 @@ export default function Home() {
           />
           <button className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
             Shorten
-          </button>
+          </button>   
+
+          
         </div> */}
 
         {/* Features */}
