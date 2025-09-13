2ebff338e94006714bf56d6a549db59ecf319cf5
aditehh <aditya.aryal888@gmail.com>
2025-09-13 21:34:17 +0545

    this

---
+++ components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                  <div className='text-white'>
+                
+                   All rights reserved.
+                    </div> 
+            </div>
+        </nav>
+
+  )
+}
+
+export default Footer
