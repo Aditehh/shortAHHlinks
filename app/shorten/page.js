b7a919d66f2930d9eddad923343d0b82430b88de
aditehh <aditya.aryal888@gmail.com>
2025-09-14 09:36:42 +0545

    this

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -3,18 +3,16 @@ import React from 'react'
 import { useState } from 'react'
 
 
-
-
-
-
 const handlechange = () => {
 
 }
 
 const page = () => {
+
     const [url, seturl] = useState("")
     const [shortUrl, setshortUrl] = useState("")
     const [generated, setgenerated] = useState(false)
+
     return (
         <div className='min-h-[86.7vh] flex flex-col items-center justify-center bg-gray-50 px-6'>
             <div >
@@ -26,8 +24,9 @@ const page = () => {
                 <div className="flex items-center justify-center gap-3 mb-6">
                     <input
                         type="text"
+                        value={url}
                         placeholder="Paste your long URL here..."
-                        onChange={handlechange}
+                        onChange={e => { seturl(e.target.value) }}
                         className="flex-1 px-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-sm"
                     />
 
@@ -38,11 +37,12 @@ const page = () => {
                 <div className="flex items-center justify-center gap-3 mb-6">
                     <input
                         type="text"
-                        onChange={handlechange}
+                        value={shortUrl}
+                        onChange={e => { setshortUrl(e.target.value) }}
                         placeholder="Enter the URL name"
                         className="flex-1 px-4 py-3 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 shadow-sm"
                     />
-                    <button className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
+                    <button className=" cursor-pointer px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
                         Shorten
                     </button>
 
